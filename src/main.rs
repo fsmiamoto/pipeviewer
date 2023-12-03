39ef5822bcb7266067baba84022e47b73e05ff55
Flavio Miamoto <hello@dorayaki.co>
2023-12-03 00:13:21 -0300

    add is_silent toggle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::io::{self, Read, Write};
+
+const CHUNK_SIZE: usize = 16 * 1024;
+
 fn main() {
-    println!("Hello, world!");
+    let is_silent = env::var("PV_SILENT").unwrap_or(String::new()).len() > 0;
+    let mut total_bytes = 0;
+
+    loop {
+        let mut buffer = [0; CHUNK_SIZE];
+        let num_read = match io::stdin().read(&mut buffer) {
+            Ok(0) => break,
+            Ok(x) => x,
+            Err(_) => break,
+        };
+        total_bytes += num_read;
+        io::stdout().write_all(&buffer[..num_read]).unwrap();
+    }
+    if !is_silent {
+        eprintln!("{}", total_bytes);
+    }
 }
